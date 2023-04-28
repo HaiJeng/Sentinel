ed2c5d84f1b87607f72a52745b593664068c8f8a
HaiJeng <d41276650@163.com>
2023-04-28 17:40:06 +0800

    2023-04-28 个人实现nacos和sentinel的推拉更新

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosPropertiesConfiguration.java
@@ -0,0 +1,46 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 读取nacos配置
+ */
+@ConfigurationProperties(prefix = "sentinel.nacos")
+public class NacosPropertiesConfiguration {
+    private String serverAddr;
+    private String dataId;
+    private String groupId = "SENTINEL_GROUP"; // 默认分组
+    private String namespace;
+
+    public String getServerAddr() {
+        return serverAddr;
+    }
+
+    public void setServerAddr(String serverAddr) {
+        this.serverAddr = serverAddr;
+    }
+
+    public String getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(String dataId) {
+        this.dataId = dataId;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+}
